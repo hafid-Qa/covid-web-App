7a72347d88abceb392d2c65378e21418ec7ff8cd
Hafid Qarchi <haqarchi@gmail.com>
2022-12-25 00:13:05 +0900

    added map to the stats page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,25 @@
 // Map display
 const KEY =
   "pk.eyJ1IjoiaGFmaWQxMDAiLCJhIjoiY2xidWk3MWRkMXA2ZzN3cGs0cDRlbnpzMSJ9.WFkGyQqKh7BglSV3jrSuzw";
+
+window.addEventListener("load", (event) => {
+  displayMap([-74.01084309969329, 40.71074359563446]);
+});
+const displayMap = (coordinates) => {
+  mapboxgl.accessToken = KEY;
+
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: coordinates,
+    zoom: 2,
+  });
+  map.addControl(new mapboxgl.NavigationControl());
+  if (coordinates[0] != -74.01084309969329) {
+    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  }
+};
+
+// displayMap([-74.01084309969329, 40.71074359563446]);
+
+console.log("hi");
