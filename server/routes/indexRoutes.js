799ad730a0e8af1a6240c5adfeaa80f4848a69d5
Hafid Qarchi <haqarchi@gmail.com>
2022-12-24 23:43:50 +0900

    route for stats page and map added

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
+const countriesList = require("../data/countriesName");
+const countriesListToArr = countriesList.countriesNames.split(", ").sort();
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/home", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/statistics", async (req, res) => {
+  const worldCount = await axios.get(
+    "https://covid-19.dataflowkit.com/v1/world"
+  );
+  res.render("stats", {
+    worldCount: worldCount.data,
+    countriesList: countriesListToArr,
+  });
 });
 
 module.exports = router;
