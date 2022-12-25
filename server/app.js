83e30f7b327ee4471c5fdce33e44647974915dd2
Hafid Qarchi <haqarchi@gmail.com>
2022-12-25 13:50:41 +0900

    styled the contact form

--- server/app.js
+++ server/app.js
@@ -5,11 +5,11 @@ import logger from "morgan";
 import indexRouter from "./routes/indexRoutes.js";
 // import contactFormRouter from "./routes/submitForm.js";
 const __dirname = path.resolve();
-let app = express();
-
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
+
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -4,11 +4,13 @@ import axios from "axios";
 
 import COUNTRIES from "../data/countriesNames.js";
 const countriesList = COUNTRIES.split(", ").sort();
+
 /* GET home page. */
 router.get("/home", function (req, res, next) {
   res.render("index");
 });
 
+// Get Statistics pages
 router.get("/statistics", async (req, res) => {
   const worldStats = await axios.get(
     "https://covid-19.dataflowkit.com/v1/world"
@@ -19,4 +21,8 @@ router.get("/statistics", async (req, res) => {
   });
 });
 
+// get contact US page
+router.get("/contact", (req, res) => {
+  res.render("contact");
+});
 export default router;
