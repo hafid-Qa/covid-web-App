1d3ea039e919e1a21f8f7130cecfb0080bde25a5
Hafid Qarchi <haqarchi@gmail.com>
2022-12-26 14:06:47 +0900

    updated foldeer structure

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const __dirname = path.resolve();
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views/pages"));
 
 app.set("view engine", "pug");
 
@@ -34,7 +34,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("../error");
 });
 
 export default app;
